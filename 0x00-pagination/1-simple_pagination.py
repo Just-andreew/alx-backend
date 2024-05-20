76992c36dbc4192e6edae4fdde0948909dc9d8a1
Andrew <andrewsolitei@gmail.com>
2024-05-21 00:07:10 +0300

    1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python3
-"""
-Defines class Server that paginates a database of popular baby names
+"""Simple pagination sample.
 """
 import csv
-import math
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-   
-    start, end = 0, 0
-    for i in range(page):
-        start = end
-        end += page_size
-
+    """Retrieves the index range from a given page and page size.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
     return (start, end)
 
 
@@ -23,6 +19,8 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
+        """Initializes a new Server instance.
+        """
         self.__dataset = None
 
     def dataset(self) -> List[List]:
@@ -37,14 +35,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
-
-        dataset = self.dataset()
-        data_length = len(dataset)
-        try:
-            index = index_range(page, page_size)
-            return dataset[index[0]:index[1]]
-        except IndexError:
+        """Retrieves a page of data.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
             return []
+        return data[start:end]
